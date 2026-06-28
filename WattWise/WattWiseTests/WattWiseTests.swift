3ea2b77a5d5558d5c0693ffd3eb5336b00469943
Emin Okic <emin.okic1729@gmail.com>
2026-06-27 19:42:50 -0500

    Bringing in the initial repository

---
+++ WattWise/WattWiseTests/WattWiseTests.swift
@@ -0,0 +1,19 @@
+//
+//  WattWiseTests.swift
+//  WattWiseTests
+//
+//  Created by Emin Okic on 6/27/26.
+//
+
+import Testing
+@testable import WattWise
+
+struct WattWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
