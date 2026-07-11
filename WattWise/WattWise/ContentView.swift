2540ec5621981ceb87c6799726a0e006cd717dbb
Emin Okic <emin.okic1729@gmail.com>
2026-07-11 14:13:00 -0500

    Removing the summary header

--- WattWise/WattWise/ContentView.swift
+++ WattWise/WattWise/ContentView.swift
@@ -24,7 +24,6 @@ struct ContentView: View {
             // Summary Tab
             NavigationStack {
                 EnergySummaryView(entries: entries)
-                    .navigationTitle("Summary")
                     .toolbar {
                         ToolbarItem(placement: .primaryAction) {
                             Button {
